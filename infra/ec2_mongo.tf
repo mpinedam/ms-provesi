0da399f74fc56b2890822a3ac1f4be957924a0ba
mpinedam <m.pinedam@uniandes.edu.co>
2025-12-02 09:56:29 -0500

    cachee

---
+++ infra/ec2_mongo.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "mongo" {
+  ami                         = "ami-0c101f26f147fa7fd" # Ubuntu 22 en us-east-1 (ejemplo)
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.internal.id]
+  key_name                    = var.key_name
+  associate_public_ip_address = false  # 👈 solo privada, no expone Mongo a internet
+
+  user_data = file("${path.module}/userdata/mongo.sh")
+
+  tags = {
+    Name = "mongo-ec2"
+  }
+}
