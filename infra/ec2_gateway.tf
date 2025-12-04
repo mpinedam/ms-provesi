3b1ba78d7d62b85ed6327f61595e3a99d076f8ed
mpinedam <m.pinedam@uniandes.edu.co>
2025-12-04 10:35:11 -0500

    cambio

--- infra/ec2_gateway.tf
+++ infra/ec2_gateway.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "apigateway" {
-  ami                         = data.aws_ami.ubuntu_2404.id
+  ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t3.micro"
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.traffic_api.id, aws_security_group.traffic_ssh.id]
--- infra/ec2_mongo.tf
+++ infra/ec2_mongo.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "mongo" {
-  ami                         = data.aws_ami.ubuntu_2404.id
+  ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t3.micro"
   vpc_security_group_ids      = [aws_security_group.traffic_mongo.id, aws_security_group.traffic_ssh.id]
   associate_public_ip_address = true
