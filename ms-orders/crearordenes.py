0a65daa26995b8fc7d260e16fe128c6e9692cf35
mpinedam <m.pinedam@uniandes.edu.co>
2025-12-04 14:45:07 -0500

    base de datos

--- ms-orders/crearordenes.py
+++ ms-orders/crearordenes.py
@@ -6,9 +6,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-ORDERS_SERVICE_URL = os.getenv("ORDERS_SERVICE_URL", "http://10.0.1.60:8080")
-USERS_SERVICE_URL = os.getenv("USERS_SERVICE_URL", "http://10.0.1.53:8000/api/users")
-PRODUCTS_SERVICE_URL = os.getenv("PRODUCTS_SERVICE_URL", "http://10.0.1.153:8001/api/products")
+ORDERS_SERVICE_URL = os.getenv("ORDERS_SERVICE_URL", "http://3.80.5.29:8080")
+USERS_SERVICE_URL = os.getenv("USERS_SERVICE_URL", "http://100.28.123.83:8000/api/users")
+PRODUCTS_SERVICE_URL = os.getenv("PRODUCTS_SERVICE_URL", "http://44.200.73.143:8001/api/products")
 
 fake = Faker()
 
