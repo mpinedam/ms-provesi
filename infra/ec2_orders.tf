8e61357b1c8884134ec1c16d875a35355867055f
mpinedam <m.pinedam@uniandes.edu.co>
2025-12-04 10:06:33 -0500

    sin esa cosa de subnet

--- infra/ec2_orders.tf
+++ infra/ec2_orders.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "orders" {
   ami            = "ami-0c101f26f147fa7fd"
   instance_type  = "t3.micro"
-  subnet_id      = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.traffic_apps.id, aws_security_group.traffic_ssh.id]
   associate_public_ip_address = true
   user_data      = file("${path.module}/userdata/orders.sh")
--- infra/ec2_orders_db.tf
+++ infra/ec2_orders_db.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "orders_db" {
   ami                         = "ami-0c101f26f147fa7fd"
   instance_type               = "t3.micro"
-  subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.traffic_db.id, aws_security_group.traffic_ssh.id]
   associate_public_ip_address = false
   private_ip = "10.0.1.174"
